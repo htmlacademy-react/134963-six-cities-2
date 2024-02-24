e38f72ed47c0046d071add06e6fe12f57f5b7514
kk1089 <kk1089@mail.ru>
2024-02-24 13:34:01 +0700

    3. 2Добавиk компонент Logo.

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+import { AppRoute } from '../../const';
+
+function Logo(): JSX.Element {
+  return (
+    <Link to={AppRoute.Main}>
+      <a className="header__logo-link">
+        <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+      </a>
+    </Link>
+  );
+}
+
+export default Logo;
--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 function NotFoundScreen(): JSX.Element {
   return (
     <div >
       <h1>404 NOT FOUND</h1>
       <p>Я искал высоко и низко. Не нашел</p>
       <h3>
-        <Link to={'/'}>Вернуться на главную страницу</Link>
+        <Link to={AppRoute.Main}>Вернуться на главную страницу</Link>
       </h3>
     </div>
 
