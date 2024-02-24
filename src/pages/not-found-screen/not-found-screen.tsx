e020f569622a0b4cd79dd6259d8bbb4178e4ab86
kk1089 <kk1089@mail.ru>
2024-02-24 22:50:33 +0700

    стили которые смог придумать

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 import { Helmet } from 'react-helmet-async';
+import styles from './not-found-screen.module.css';
 
 function NotFoundScreen(): JSX.Element {
   return (
-    <div >
+    <div className= {styles.notFoundScreen}>
       <Helmet>
         <title>{'6 cities - Not Found'}</title>
       </Helmet>
-      <h1>404 NOT FOUND</h1>
-      <p>Я искал высоко и низко. Не нашел</p>
-      <h3>
-        <Link to={AppRoute.Main}>Вернуться на главную страницу</Link>
+      <h1 className={styles.title}>404 NOT FOUND</h1>
+      <p className={styles.text}>Я искал высоко и низко. Не нашел</p>
+      <h3 >
+        <Link className={styles.link} to={AppRoute.Main}>Вернуться на главную страницу</Link>
       </h3>
     </div>
 
