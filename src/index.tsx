5ae6b8c9542088fe9635d8f666c4be8c80f25104
kk1089 <kk1089@mail.ru>
2024-04-17 22:51:33 +0700

    опечатка исправлено

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
-import { reviews } from './mocks/reviewes';
+import { reviews } from './mocks/reviews';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
