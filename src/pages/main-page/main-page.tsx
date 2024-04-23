77f2f64c91513be4f394b06ea07e2cb01ee4831e
kk1089 <kk1089@mail.ru>
2024-04-24 02:34:41 +0700

    main-empty добавил

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,12 +2,20 @@ import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import Locations from '../../components/locations/locations';
 import Cities from '../../components/cities/cities';
+import { useSelector } from 'react-redux';
+import MainEmpty from '../../components/main-empty/main-empty';
+import { FullOffer } from '../../types/offer';
+
+type RootState = {
+  offers: FullOffer[];
+}
 
 
 function MainPage (): JSX.Element {
+  const offers = useSelector((state: RootState) => state.offers);
 
   return (
-    <div className="page page--gray page--main">
+    <div className={`page__main page__main--index ${offers.length === 0 ? 'page__main--index-empty' : ''}`}>
       <Helmet>{'6 cities - Main'}</Helmet>
       <Header />
 
@@ -17,7 +25,7 @@ function MainPage (): JSX.Element {
           <Locations />
         </div>
         <div className="cities">
-          <Cities />
+          {offers.length === 0 ? <MainEmpty /> : <Cities />}
         </div>
       </main>
     </div>
