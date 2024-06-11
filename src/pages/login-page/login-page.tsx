4eb01a1f2ca2365a77cfdf9179d33b6a298cc219
kk1089 <kk1089@mail.ru>
2024-06-11 22:19:18 +0700

    добавил рандомную локацию в loginPage

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -2,8 +2,15 @@
 import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import LoginSection from '../../components/login-section/login-section';
+import { AppRoute, LOCATIONS } from '../../const';
+import { Link } from 'react-router-dom';
+import { getRandomLocation } from '../../utils/utils';
+import { useAppDispatch } from '../../hooks';
+import { setCity } from '../../redux/slices/ui/ui';
 
 function LoginPage() {
+  const dispatch = useAppDispatch();
+  const randomLocation = getRandomLocation(LOCATIONS);
   return (
     <div className="page page--gray page--login">
       <Helmet>{'6 cities - Login'}</Helmet>
@@ -14,9 +21,9 @@ function LoginPage() {
           <LoginSection />
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
-              </a>
+              <Link to={AppRoute.Main} className="locations__item-link" onClick={() => dispatch(setCity(randomLocation))}>
+                <span>{randomLocation}</span>
+              </Link>
             </div>
           </section>
         </div>
